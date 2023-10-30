21ef7f495757b8f1c600e71f2be9906f15883e77
roldane <2030yaer@gmail.com>
2023-10-30 22:06:19 +0200

    Add files via upload

---
+++ lab5_kovalevskyi.py
@@ -0,0 +1,35 @@
+print(
+    """"
+    Лабораторна робота №5
+Мета: освоїти роботу зі списками.
+
+     Завдання 1
+ """)
+
+list = [1, 2, int(input("num - ")), 4, 5]
+list.pop()
+print("список - ", list, "довжина - ", len(list))
+print(" ")
+
+
+print("     Завдання 2")
+print("Несортоване:")
+list = [1, 5, 3, 4, 2, 6]
+print(list)
+print("Відсортоване:")
+j = 1
+while j < len(list):
+    k = 0
+    while k < (len(list) - 1):
+        if list[k] > list[k + 1]:
+            list[k], list[k + 1] = list[k + 1], list[k]
+        k += 1
+    j += 1
+
+print(list)
+print(" ")
+
+
+print("     Завдання 3")
+list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+
