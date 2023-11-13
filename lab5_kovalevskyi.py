ed1805c5cbc518c3f54053ba8348343e51d3256e
roldane <2030yaer@gmail.com>
2023-11-13 20:44:44 +0200

    Add files via upload

--- lab5_kovalevskyi.py
+++ lab5_kovalevskyi.py
@@ -32,4 +32,24 @@ print(" ")
 
 print("     Завдання 3")
 list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+j = 1
+while j < len(list):
+    k = 0
+    while k < (len(list) - 1):
+        if list[k] > list[k + 1]:
+            list[k], list[k + 1] = list[k + 1], list[k]
+        k += 1
+    j += 1
+r = [list[0]]
+i = 1
+j = 0
+while i < len(list):
+    if r[j] != list[i]:
+        r.append(list[i])
+        j += 1
+    i += 1
+print("The list with unique elements only:")
+print(r)
 
+
+print("     Завдання 4")
